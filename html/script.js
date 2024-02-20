70078861dbd276256389c644905220d51238b589
MGAviles25 <160518131+MGAviles25@users.noreply.github.com>
2024-02-20 20:05:00 +0000

    cadastro

--- html/script.js
+++ html/script.js
@@ -1,3 +1,13 @@
+document.getElementById("cadastroForm").addEventListener("submit", function(event){
+  event.preventDefault();
+  let formData = new FormData(this);
+  let formObject = {};
+  formData.forEach(function(value, key){
+      formObject[key] = value;
+  });
+  console.log(formObject);
+
+});
 
 const form = document.getElementById('formpagina');
 
