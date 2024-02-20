8e8053d7916e233456095956818772c9b109da7a
MGAviles25 <160518131+MGAviles25@users.noreply.github.com>
2024-02-20 17:50:49 +0000

    script

---
+++ html/script.js
@@ -0,0 +1,65 @@
+
+const form = document.getElementById('formpagina');
+
+function validar() {
+    if(form.checkvalidity(e) === false) {
+        form.classList.add('was-validated');
+        e.preventDefault();
+        e.stopPropagation();
+        
+    } else {
+        form.classList.remove('was-validated');
+        
+    }
+}
+
+form.addEventListener('submit', validar);
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const eventos = [
+      {
+        id: 1,
+        nome: 'Corinthians X Palmeiras',
+        data: '04/04/2024',
+        local: 'Maracanã, Rio de Janeiro - RJ',
+        preco: 'R$ 50,00'
+      },
+      {
+        id: 2,
+        nome: 'Show Zé Ramalho',
+        data: '05/05/2024',
+        local: 'Bosque Marechal Cândido Rondon, Londrina - PR',
+        preco: 'R$ 100,00'
+      },
+      {
+        id: 3,
+        nome: 'Show Danilo Gentili',
+        data: '06/06/2024',
+        local: 'Teatro municipal, Presidente Prudente - SP',
+        preco: 'R$ 150,00'
+      },
+      {
+        id: 4,
+        nome: 'Turne Leo Lins 4',
+        data: '07/07/2024',
+        local: 'Teatro municipal, Marília - SP',
+        preco: 'R$ 200,00'
+      }
+    ];
+  
+    const eventosContainer = document.querySelector('.eventos');
+  
+    eventos.forEach(evento => {
+      const eventoElemento = document.createElement('div');
+      eventoElemento.className = 'evento';
+      eventoElemento.innerHTML = `
+        <h2>${evento.nome}</h2>
+        <p>Data: ${evento.data}</p>
+        <p>Local: ${evento.local}</p>
+        <p>Preço: ${evento.preco}</p>
+        <button onclick="window.location.href='/evento/${evento.id}'">Ver detalhes</button>
+      `;
+      eventosContainer.appendChild(eventoElemento);
+    });
+  });
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import process from 'express';
+import path from 'path';
 
 const host= "0.0.0.0";
 const porta= 3000;
 
 const app = express();
+
+app.use(express.static(path.join(process.cwd(), 'html')))
 
 app.listen(porta, host, ()=>{
     console.log('Servidor escutando em http://${host}:${porta}')
