f9d9e04c5572212bbc522421a365c2a2ed91a3e3
MGAviles25 <160518131+MGAviles25@users.noreply.github.com>
2024-02-21 05:57:07 +0000

    scr

--- cadastro/scriptC.js
+++ cadastro/scriptC.js
@@ -8,3 +8,40 @@ document.getElementById("cadastroForm").addEventListener("submit", function(even
     console.log(formObject);
     
 });
+
+function validarCadastro() {
+    let nome = document.getElementById("nome");
+    let email = document.getElementById("email");
+    let senha = document.getElementById("senha");
+    let isValid = true;
+
+    if (nome.value.trim() === "") {
+      nome.classList.add("invalid");
+      isValid = false;
+    } else {
+      nome.classList.remove("invalid");
+    }
+
+    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email.value.trim())) {
+      email.classList.add("invalid");
+      isValid = false;
+    } else {
+      email.classList.remove("invalid");
+    }
+
+    if (senha.value.trim().length < 6) {
+      senha.classList.add("invalid");
+      isValid = false;
+    } else {
+      senha.classList.remove("invalid");
+    }
+
+    return isValid;
+  }
+
+  function validarTelefone(telefone) {
+
+    let regex = /^\([0-9]{2}\) [0-9]{4,5}-[0-9]{4}$/;
+    return regex.test(telefone);
+}
